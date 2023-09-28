2eab750de8751bb79196e67aa8a6cd012bfd0b3e
Álex <alejandro.mcamps@bosonit.com>
2023-09-28 11:26:31 +0200

    Cambiar mensaje etiqueta label

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, OnInit, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,11 +7,17 @@ import { Directive, ElementRef, OnInit, Input } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
-  private _color = 'red'
+  private _color: string  = 'red';
+  private _errors? : ValidationErrors | null; 
 
   @Input() set color( value: string){
     this._color = value;
     this.setSTyle();
+  }
+
+  @Input() set errors (value: ValidationErrors | null | undefined){
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -27,6 +34,21 @@ export class CustomLabelDirective implements OnInit {
     if (!this.htmlElement)return;
 
 this.htmlElement!.nativeElement.style.color = this._color
+  }
+
+  setErrorMessage():void {
+    if(!this.htmlElement) return;
+    if ( !this._errors) {
+      this.htmlElement.nativeElement.innerText = ' No hay errores ';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if( errors.includes('required')){
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
   }
 
 }
